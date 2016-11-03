02650e5c29b0b1eb773865b5984bd8acfd86c38f
Lily Vang <lily.sf888@gmail.com>
2016-11-03 13:58:26 -0700

    working on switch case statement

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -5,7 +5,7 @@ var actions = require('../actions/index');
 var initialState = {
   userGuess: '',
   allGuesses: [],
-  feedback: '',
+  feedback: 'hi',
   randomNum: Math.floor(Math.random() * 100),
   guessAttempts: 0,
   difference: 0
@@ -26,44 +26,46 @@ var reducers = function(state = initialState, action) {
       var feedback = state.feedback;
       var difference = state.difference;
       difference = Math.abs(randomNum - userGuess);
-    
+
       // switch statement to say how close to guess
-       var getFeedback = function(feedback, difference) {
-         var feedback = feedback;
-         var difference = difference;
-         console.log(feedback);
+       var getFeedback = function(feedback,difference) {
+         feedback = "bye";
+
+         //console.log(feedback);
          console.log(difference);
-          switch(difference){
+           switch(difference){
 
-                case difference > 50:
-                  feedback = 'ice cold';
+                 case difference > 50:
+                   return feedback = 'ice cold';
+                    break;
+                 case difference > 30 && difference < 50:
+                   return feedback = 'cold';
                    break;
-                case difference > 30 && difference < 50:
-                  feedback = 'cold';
-                  break;
-                case difference > 20 && difference < 30:
-                  feedback = 'warm';
-                  break;
-                case difference > 10 && difference < 20:
-                  feedback = 'hot';
-                  break;
-                case difference > 1 && difference < 10:
-                  feedback = 'very hot';
-                  break;
-                case difference === 0:
-                  feedback = 'Congrats, you guessed the right number';
-               }
-               return feedback;
-      }
-       getFeedback(difference, feedback);
+                 case difference > 20 && difference < 30:
+                   return feedback = 'warm';
+                   break;
+                 case difference > 10 && difference < 20:
+                   return feedback = 'hot';
+                   break;
+                 case difference > 1 && difference < 10:
+                    return feedback = 'very hot';
+                   break;
+                 case difference === 0:
+                   return feedback = 'Congrats, you guessed the right number';
+                }
+             return feedback;
+                //console.log(feedback);
 
+      }
+       var feedback = getFeedback(feedback,difference);
+       //console.log(feedback);
 
       var newState = Object.assign({}, state, {
         userGuess: userGuess,
         allGuesses: state.allGuesses.concat(userGuess),
         guessAttempts: state.allGuesses.length + 1,
-        feedback: feedback,
-        difference: difference
+        feedback: state.feedback,
+        difference: state.difference
       });
       //console.log(feedback)
       //console.log(difference)
