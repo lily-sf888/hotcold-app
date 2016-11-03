7ad077f387e538d7aed85aea728303c3bb3d10d8
Lily Vang <lily.sf888@gmail.com>
2016-11-03 15:24:28 -0700

    switched to if else statement

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -5,7 +5,7 @@ var actions = require('../actions/index');
 var initialState = {
   userGuess: '',
   allGuesses: [],
-  feedback: 'hi',
+  feedback: '',
   randomNum: Math.floor(Math.random() * 100),
   guessAttempts: 0,
   difference: 0
@@ -23,58 +23,38 @@ var reducers = function(state = initialState, action) {
       var userGuess = action.userGuess;
       userGuess = parseInt(userGuess);
       var randomNum = state.randomNum;
-      var feedback = state.feedback;
-      var difference = state.difference;
+      var feedback = feedback;
+      var difference = difference;
       difference = Math.abs(randomNum - userGuess);
 
-      // switch statement to say how close to guess
-       var getFeedback = function(feedback,difference) {
-         feedback = "bye";
-
-         //console.log(feedback);
-         console.log(difference);
-           switch(difference){
-
-                 case difference > 50:
-                   return feedback = 'ice cold';
-                    break;
-                 case difference > 30 && difference < 50:
-                   return feedback = 'cold';
-                   break;
-                 case difference > 20 && difference < 30:
-                   return feedback = 'warm';
-                   break;
-                 case difference > 10 && difference < 20:
-                   return feedback = 'hot';
-                   break;
-                 case difference > 1 && difference < 10:
-                    return feedback = 'very hot';
-                   break;
-                 case difference === 0:
-                   return feedback = 'Congrats, you guessed the right number';
-                }
-             return feedback;
-                //console.log(feedback);
-
-      }
-       var feedback = getFeedback(feedback,difference);
-       //console.log(feedback);
+      //get feedback to users how close their guess is
+      var getFeedback = function(feedback, difference) {
+        if(difference > 50) {
+  			     feedback = "ice cold";
+  			}else if(difference > 30 && difference < 50 ) {
+  			     feedback = "cold";
+  			}else if(difference > 20 && difference < 30) {
+  			     feedback = "warm";
+  			}else if(difference > 10 && difference < 20) {
+  			     feedback ="hot";
+  			}else if(difference > 1 && difference < 10) {
+  			     feedback = "very hot";
+  			}else if(difference === 0) {
+  				feedback = "Congrats, you guessed the right number!";
+  			}
+        return feedback;
+        }
+        var feedback = getFeedback(feedback, difference);
 
       var newState = Object.assign({}, state, {
         userGuess: userGuess,
         allGuesses: state.allGuesses.concat(userGuess),
         guessAttempts: state.allGuesses.length + 1,
-        feedback: state.feedback,
-        difference: state.difference
+        feedback: feedback,
+        difference: difference
       });
-      //console.log(feedback)
-      //console.log(difference)
       console.log(newState)
       return newState
-
-
-
-
   }
   return state;
 };
